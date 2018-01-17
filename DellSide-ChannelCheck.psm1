008299057f8bae814bfdd9d3bd7729ed0e69a615
Chris Taylor <chris@christaylor.rocks>
2018-01-17 00:33:48 -0700

    Update DellSide-ChannelCheck.psm1

--- DellSide-ChannelCheck.psm1
+++ DellSide-ChannelCheck.psm1
@@ -30,39 +30,58 @@ param (
     [parameter(ValueFromPipeline)]
     $Model
     )
-    if(!$Model){
-        $Model = (Get-WmiObject Win32_Computersystem | Where {$_.manufacturer -like "*Dell*"}).model
-        if (!$Model) {
-            Write-Warning "ERROR: Dell model not detected. You can specify a model with the -Model switch."
-            exit 1
+    begin{
+        # Get list of hardware
+        try {
+            $test = iwr $url -ErrorAction Stop
+        } catch { 
+            Write-Warning "There was an error accessing the Dell hardware list."
         }
-        $Local = $true
     }
-    $_Model = ($Model -replace 'non-vPro', '').trim()
-    $_Model = $_Model -replace ' Tower',''
-    $_Model = $_Model -replace ' ','.*'
-    $url = 'http://www.dell.com/support/article/us/en/04/sln308587/microprocessor-side-channel-vulnerabilities-cve-2017-5715-cve-2017-5753-cve-2017-5754-impact-on-dell-products?lang=en'
-    $test = iwr $url
-    $Contetnt = $test.ParsedHtml.getElementById('dvDynamicContent')
-    $PlainText = $Contetnt.outerText
-
-    $Split = ($PlainText -split '\n\s*\n\s*\n')
-    try {
-        $Min = ((($Split | select-string -pattern $_Model) -split '[\r\n]') |? {$_})[1]
-        if ($Local ) {
-            $Current = (Get-WmiObject win32_bios).SMBIOSBIOSVersion
-            $Output = @{
-                "Model" = $Model
-                "Minimum Version" = $Min
-                "Current Version" = $Current
-                "Protected" = $Current -ge $Min
+    process {
+        # If no model is specified check local machine
+        if(!$Model){
+            $Local = $true
+            $Model = (Get-WmiObject Win32_Computersystem | Where {$_.manufacturer -like "*Dell*"}).model
+            if (!$Model) {
+                Write-Warning "ERROR: Dell model not detected. You can specify a model with the -Model switch."
+                exit 1
             }
-        } else {
-            $Output = $Min
         }
+        # Clean up the model a bit for the search
+        try {
+            $_Model = ($Model -replace 'non-vPro', '').trim()
+            $_Model = $_Model -replace ' Tower',''
+            $_Model = $_Model -replace ' ','.*'
+            $Contetnt = $test.ParsedHtml.getElementById('dvDynamicContent')
+            $PlainText = $Contetnt.outerText
+            $Split = ($PlainText -split '\n\s*\n\s*\n')
+        } catch {
+            Write-Warning "ERROR: There was an issue parsing the web data."
+        }
+        # Check for match
+        try {
+            $Min = ((($Split | select-string -pattern $_Model) -split '[\r\n]') |? {$_})[1]
+            if ($Local) {
+                $Current = (Get-WmiObject win32_bios).SMBIOSBIOSVersion
+                $Output = @{
+                    "Model" = $Model
+                    "Minimum Version" = $Min
+                    "Current Version" = $Current
+                    "Protected" = $Current -ge $Min
+                }
+            } else {
+                $Output = @{
+                    "Model" = $Model
+                    "Minimum Version" = $Min
+                }
+            }
         
-        Write-Output $Output
-    } catch {
-        Write-Warning "ERROR: Unable to find model."
+            Write-Output $Output
+        } catch {
+            Write-Warning "ERROR: Unable to find model: $Model"
+        }
+    }
+    end {
     }
 }
